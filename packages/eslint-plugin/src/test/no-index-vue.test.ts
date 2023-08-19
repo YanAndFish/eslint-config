e3e091ac5952442732ba13f50338c290933ebe99
YanAndFish <40020926+YanAndFish@users.noreply.github.com>
2023-08-19 13:58:41 +0800

    feat: add `no-index-vue` unit test

---
+++ packages/eslint-plugin/src/test/no-index-vue.test.ts
@@ -0,0 +1,28 @@
+import { RuleTester } from '@typescript-eslint/rule-tester'
+import rule, { name } from '../rules/no-index-vue'
+
+// should pass
+const valids = ['src/foo.vue', 'src/fooIndex.vue', 'src/index.ts']
+// should fail
+const invalids = [
+  'src/index.vue',
+  'index.vue',
+  'src/Index.vue',
+  'src/Index.vue',
+]
+
+const ruleTester: RuleTester = new RuleTester({
+  parser: require.resolve('@typescript-eslint/parser'),
+})
+
+ruleTester.run(name, rule as any, {
+  valid: valids.map((e) => ({
+    code: '',
+    filename: e,
+  })),
+  invalid: invalids.map((e) => ({
+    code: '',
+    filename: e,
+    errors: [{ messageId: 'noIndexVue' }],
+  })),
+})
